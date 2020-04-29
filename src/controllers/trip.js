79886a859fdee038c8e5d0ced8775d54d7cb5802
ansnekit <ansnekit@gmail.com>
2020-04-29 21:57:44 +0300

    Исправлены замечания наставника

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -5,11 +5,54 @@ import EditFormComponent from '../components/edit-form.js';
 
 import {render, renderPosition, replace, remove} from '../utils/render.js';
 
+const renderEvent = (container, point) => {
+  const replaceTaskToEdit = () => {
+    replace(editFormComponent, pointComponent);
+  };
+
+  const replaceEditToTask = () => {
+    replace(pointComponent, editFormComponent);
+  };
+
+  const onEscKeyDown = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      replaceEditToTask();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  const pointComponent = new PointComponent(point);
+  pointComponent.setDownButtonClickHandler(() => {
+    replaceTaskToEdit();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+
+  const editFormComponent = new EditFormComponent(point);
+  editFormComponent.setSubmitHandler((evt) => {
+    evt.preventDefault();
+    replaceEditToTask();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  });
+
+  editFormComponent.setDeleteClickhandler(() => {
+    remove(editFormComponent);
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  });
+
+  editFormComponent.setUpClickhandler(() => {
+    replaceEditToTask();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  });
+
+  render(container, pointComponent, renderPosition.BEFOREEND);
+};
+
 const renderTripDays = (container, routePoints) => {
   const days = new Map();
 
-  routePoints
-  .slice()
+  routePoints.slice()
   .sort((a, b) => (Date.parse(a.dateFrom) - Date.parse(b.dateFrom)))
   .forEach((it) => {
     const dateTime = Date.parse(it.dateFrom);
@@ -23,75 +66,28 @@ const renderTripDays = (container, routePoints) => {
     }
 
     return days;
-
   });
 
   const daysList = new DaysListComponent();
 
   render(container, daysList, renderPosition.BEFOREEND);
 
-  // console.log(days);
-
   let count = 0;
-
-  days.forEach((day, key) => {
+  days.forEach((event, key) => {
     count++;
 
     const tripDay = new DayComponent(key, count);
     const eventsList = tripDay.getElement().querySelector(`.trip-events__list`);
-    // const points = day;
 
     render(daysList.getElement(), tripDay, renderPosition.BEFOREEND);
 
-    day.map((point) => {
-      const replaceTaskToEdit = () => {
-        replace(editFormComponent, pointComponent);
-      };
-
-      const replaceEditToTask = () => {
-        replace(pointComponent, editFormComponent);
-      };
-
-      const onEscKeyDown = (evt) => {
-        const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-        if (isEscKey) {
-          replaceEditToTask();
-          document.removeEventListener(`keydown`, onEscKeyDown);
-        }
-      };
-
-      const pointComponent = new PointComponent(point);
-      pointComponent.setDownButtonClickHandler(() => {
-        replaceTaskToEdit();
-        document.addEventListener(`keydown`, onEscKeyDown);
-      });
-
-      const editFormComponent = new EditFormComponent(point);
-
-      editFormComponent.setSubmitHandler((evt) => {
-        evt.preventDefault();
-        replaceEditToTask();
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      });
-
-      editFormComponent.setDeleteClickhandler(() => {
-        remove(editFormComponent);
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      });
-
-      editFormComponent.setUpClickhandler(() => {
-        replaceEditToTask();
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      });
-
-      return render(eventsList, pointComponent, renderPosition.BEFOREEND);
+    event.forEach((point) => {
+      renderEvent(eventsList, point);
     });
 
   });
 
 };
-
 
 export default class TripController {
   constructor(container) {
